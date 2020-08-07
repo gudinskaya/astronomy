1178c04ced3b4e7d7c87ff138cadd8f2eff4f0fc
Anna Gudinskaya <ann.gudin@gmail.com>
2020-08-07 14:02:59 +0300

    Updates

---
+++ precache-manifest.bdf11113411846885d596fa8d87f4ee8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b2a40e773c9aa01b9125252acc9407",
+    "url": "/astronomy/index.html"
+  },
+  {
+    "revision": "c9d1213e0fa4d5c8a216",
+    "url": "/astronomy/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "eb5888fc9b8f500471d2",
+    "url": "/astronomy/static/css/main.e6286712.chunk.css"
+  },
+  {
+    "revision": "c9d1213e0fa4d5c8a216",
+    "url": "/astronomy/static/js/2.13e82a1f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/astronomy/static/js/2.13e82a1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb5888fc9b8f500471d2",
+    "url": "/astronomy/static/js/main.5312ce34.chunk.js"
+  },
+  {
+    "revision": "ca83fb24016c23dbfffb",
+    "url": "/astronomy/static/js/runtime-main.e43f9e17.js"
+  },
+  {
+    "revision": "59d9c495a21f13ce4380b88d2729dfd3",
+    "url": "/astronomy/static/media/cosmos.59d9c495.jpg"
+  }
+]);
